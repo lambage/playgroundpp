f3f467a6fa051ba695013abdedc4177fdf7b7a25
Kris Lamb <kris@adhawkmicrosystems.com>
2023-06-01 22:21:01 -0400

    Initial commit
    The result is surprising, I expected to see more variation in thread
    execution, but it seems to do a bunch of work in one thread before
    another thread has chance to run.  My current suspicion without doing
    any futher digging in is just that because it's not a real world example
    the lock is held for the majority of the "work" so other threads are
    just waiting on the mutex.  I could probably achieve what I was looking
    at by doing some extra work after the mutex unlocks.

---
+++ src/main.cpp
@@ -0,0 +1,56 @@
+#include <boost/asio.hpp>
+#include <thread>
+#include <iostream>
+#include <CLI/CLI.hpp>
+
+int main(int argc, char *argv[])
+{
+    CLI::App app;
+
+    int num_threads = 4;
+    app.add_option("-n", num_threads, "Number of threads to run");
+
+    int jobs = 10000;
+    app.add_option("-j", jobs, "Number of jobs to run");
+
+    CLI11_PARSE(app, argc, argv);
+
+    boost::asio::io_context context;
+    auto work = boost::asio::make_work_guard(context);
+    std::mutex print_mutex;
+
+    std::vector<std::thread> threads;
+
+    for (auto i = 0; i < num_threads; i++)
+    {
+        threads.emplace_back([&context]{ context.run(); });
+    }
+
+    std::vector<std::future<void>> futures;
+    for (auto i = 0; i < jobs; i++)
+    {
+        auto promise = std::make_shared<std::promise<void>>();
+        futures.emplace_back(promise->get_future());
+        context.post([i, promise = std::move(promise), &print_mutex](){
+            {
+                std::lock_guard<std::mutex> lock(print_mutex);
+                std::cout << "Hello world\t" << i << "\t" << std::this_thread::get_id() << "\n";
+            }
+            promise->set_value();
+        });
+    }
+
+    for (auto &future : futures)
+    {
+        future.get();
+    }
+
+    context.stop();
+
+    for (auto &t : threads)
+    {
+        t.join();
+    }
+
+    return 0;
+}
